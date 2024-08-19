32e5b031f693db8ef94099c00ca5d1bfd44b774f
Vape1337 <wunans@foxmail.com>
2024-08-19 09:40:34 +0800

    update

--- VapeV4.10/open.py
+++ VapeV4.10/open.py
@@ -3,11 +3,9 @@ import subprocess
 import tkinter as tk
 import threading
 import os
-import psutil
-import time
+from time import sleep
 
 load_process = None
-paused_event = threading.Event()
 
 def filter_content():
     try:
@@ -48,45 +46,13 @@ def check_for_stage(output):
     global load_process
     if load_process is None:
         return
-    
-    pid = load_process.pid
-    
-    if "stage: 21" in output:
-        print("检测到 stage: 21，程序将进入未响应状态")
-        suspend_process(pid)
-        paused_event.clear()  # 设置阻塞
-        paused_event.wait()    # 程序挂起，等待信号解除
-    elif "stage: 28" in output:
-        print("检测到 stage: 28，程序将恢复响应")
-        resume_process(pid)
-        paused_event.set()     # 恢复响应
-
-def suspend_process(pid):
-    process = psutil.Process(pid)
-    for thread in process.threads():
-        thread_handle = ctypes.windll.kernel32.OpenThread(0x2, False, thread.id)
-        if thread_handle:
-            ctypes.windll.kernel32.SuspendThread(thread_handle)
-            ctypes.windll.kernel32.CloseHandle(thread_handle)
-    print(f"已暂停进程 {pid} 的所有线程")
-
-def resume_process(pid):
-    process = psutil.Process(pid)
-    for thread in process.threads():
-        thread_handle = ctypes.windll.kernel32.OpenThread(0x2, False, thread.id)
-        if thread_handle:
-            ctypes.windll.kernel32.ResumeThread(thread_handle)
-            ctypes.windll.kernel32.CloseHandle(thread_handle)
-    print(f"已恢复进程 {pid} 的所有线程")
 
 def start_load_exe():
     global load_process
+    sleep(15)
     load_path = "load.exe"
     if os.path.exists(load_path):
         load_process = subprocess.Popen([load_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print("已启动 load.exe")
-    else:
-        print("未找到 load.exe 文件")
 
 def on_inject_button_click():
     global load_process
